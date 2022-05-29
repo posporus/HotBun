a1a672f7e37166052fc732d8eae1477ce48239e2
Erik Siefken <git@posporus.de>
2022-05-29 11:52:53 +0200

    counter example

--- examples/test/www/home.tsx
+++ examples/test/www/home.tsx
@@ -1,14 +1,27 @@
 /** @jsxImportSource https://esm.sh/preact */
 
 import { h, Component, render } from "https://esm.sh/preact@10.7.2"
+import { useState } from "https://esm.sh/preact@10.7.2/hooks"
 const name = 'Hammelheini'
-function MyComponent(props:any) {
+function MyComponent (props: any) {
     return <div>My name is {props.name}.</div>;
-  }
+}
 
-  const test = ()=>{
-      alert('i bims '+name)
-  }
+const test = () => {
+    alert('i bims ' + name)
+}
+
+function Counter () {
+    const [value, setValue] = useState(0);
+
+    return (
+        <div>
+            <div>Counter: {value}</div>
+            <button onClick={() => setValue(value + 1)}>Increment</button>
+            <button onClick={() => setValue(value - 1)}>Decrement</button>
+        </div>
+    )
+}
 
 const App = () => {
     return (
@@ -16,10 +29,11 @@ const App = () => {
             <h1>hello world</h1>
             <div>this is a text</div>
             <MyComponent name={name}></MyComponent>
+            <Counter></Counter>
             <button onClick={test}> click</button>
-            
+
         </div>
     )
 }
 
-export {App}
+export { App }
